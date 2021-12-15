41b524a64750aec6542807ed12c95683d3119e14
NarendraKumarSeelam <snarendrakumar182@gmail.com>
2021-12-15 14:05:48 +0530

    Initial Commit

---
+++ src/main/java/com/narendra/springboot2openapi/SpringBoot2OpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.narendra.springboot2openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2OpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2OpenApiApplication.class, args);
+	}
+
+}
